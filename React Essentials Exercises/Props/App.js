5f2b1b132b6c6b2cd770455b3268185a0d0a811c
sonyerg <ericksonrguinto@gmail.com>
2024-02-13 21:02:35 +1300

    added props and event handlers exercises

---
+++ React Essentials Exercises/Props/App.js
@@ -0,0 +1,25 @@
+export function CourseGoal({ title, description }) {
+  return (
+    <li>
+      <h2>{title}</h2>
+      <p>{description}</p>
+    </li>
+  );
+}
+
+function App() {
+  return (
+    <div id="app" data-testid="app">
+      <h1>Time to Practice</h1>
+      <p>One course, many goals! 🎯</p>
+      <ul>
+        {/* OUTPUT AT LEAST TWO CourseGoal components here */}
+        {/* One of them should have a title of “Learn React” and a description of “In-depth” */}
+        <CourseGoal title="Learn React" description="In-depth" />
+        <CourseGoal title="Learn Next-JS" description="In-depth" />
+      </ul>
+    </div>
+  );
+}
+
+export default App;
